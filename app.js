f8852f12f5dcec7fc9d58e6171962353ea7eae3b
HildaAb <ilona.aase@gmail.com>
2025-02-05 19:47:08 +0100

    added function to prev

---
+++ app.js
@@ -0,0 +1,15 @@
+const imageContainerEl = document.querySelector(".image-container");
+
+const prevEl = document.querySelector("#prev");
+const nextEl = document.querySelector("#next");
+
+let x = 0;
+
+prevEl.addEventListener("click", () => {
+  x = x + 45;
+  updateGallery();
+});
+
+function updateGallery() {
+  imageContainerEl.style.transform = `perspective(1000px)rotateY(${x}deg)`;
+}
